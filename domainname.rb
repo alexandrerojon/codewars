ff99dfee2e85e73364c3c0bcc2b9608a60248276
Alexandre Rojon <alexandre.rojon@gmail.com>
2022-01-02 23:10:23 +0100

    Domain name challenge

---
+++ domainname.rb
@@ -0,0 +1,9 @@
+def domain_name(url)
+    # return just the domain name
+end
+  
+
+domain_name("http://google.com")
+domain_name("http://google.co.jp") 
+domain_name("www.xakep.ru")
+domain_name("https://youtube.com")
