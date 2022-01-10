a1649b6a20ec776e7a81ffe0bce15a7d2a3521d3
Alexandre Rojon <alexandre.rojon@gmail.com>
2022-01-10 21:46:57 +0100

    Stong increment refactory

--- stringincrement.rb
+++ stringincrement.rb
@@ -12,24 +12,44 @@
 
 def increment_string(input)
     #your code here
-    items = input.split("")
-    count = 0
-    for i in items do
-        i.to_i
-        count += 1 if i.match(/\d/)
-    end 
-    given_numbers = items.select {|i| i.match(/\d/) }
-    puts given_numbers
-    if given_numbers
+    puts input
+    items = input.split('')
+    length_of_input = (items.length - 1)
+    total_integers = items.select {|item| item.match(/\d/)}
+    case total_integers.length
+        when 3
+            all_numbers = items.select {|item| item.match(/\d/) }.join()
+            puts all_numbers.
+         
 
+           # if items.last.to_i < 9
+            #    items[length_of_input] = items.last.to_i + 1
+           # elsif items.last == 0
+             #   items[length_of_input - 1] = items[length_of_input - 1 ].to_i + 1
+            #end
+        when 2 
+            # all_numbers = items.select {|item| item.match(/\d/) }.join()
 
+            if items.last.to_i < 9
+                items[length_of_input] = items.last.to_i + 1
+            elsif items.last == 0
+                items[length_of_input - 1] = items[length_of_input - 1 ].to_i + 1
+            end
+        when 1
+            all_numbers = items.select {|item| item.match(/\d/) }.join()
+
+        when 0
+            return input
+    end
+
+    return items.join("")
 end
 
 
 
 #increment_string("foo")
-puts increment_string("foobar001")
+increment_string("foobar029")
 #increment_string("foobar1")
-#increment_string("foobar00") 
+increment_string("foobar00") 
 #increment_string("foobar99") 
 #increment_string("")
