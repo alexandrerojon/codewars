a004b165d9e67689590a48f2e9805d321fc26f30
Alexandre Rojon <alexandre.rojon@gmail.com>
2022-01-27 19:00:56 +0100

    Error running test cases

--- scrambles.rb
+++ scrambles.rb
@@ -14,23 +14,35 @@ scramble('katas', 'steak') ==> False
 
 
 def scramble(s1,s2)
-    first_word = s1.split('').select { |item| s2.include?   item }
-    first_word = first_word.sort
+    first_word = []
     second_word = s2.split('')
+
+    s1.split('').select { |item| 
+        if second_word.include? item 
+            first_word << item
+            second_word.delete(item)
+        end
+    }
+    first_word = first_word.sort
     second_word = second_word.sort
-  
-    if first_word.join() == second_word.join()
-      return true
+
+    third_word = s2.split('').sort
+    puts "----"
+    puts first_word.join()
+    puts third_word.join()
+    if first_word.join() == third_word.join()
+        return true
     else
-      return false  
+        return false  
     end
-  
-  end
+
+end
 
 
 # Test cases
-#puts scramble('rkqodlw','world')
+
+puts scramble('rkqodlw','world')
 puts scramble('cedewaraaossoqqyt','codewars')
-#puts scramble('katas','steak')
-#puts scramble('scriptjava','javascript')
-#puts scramble('scriptjava','javascript')
+puts scramble('katas','steak')
+puts scramble('scriptjava','javascript')
+puts scramble('scriptjava','javascript')
