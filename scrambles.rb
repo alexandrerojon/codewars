34a1096784033a7abd357fed0ac0f5c9a9b92df1
Alexandre Rojon <alexandre.rojon@gmail.com>
2022-01-27 17:25:11 +0100

    Test cases added

--- scrambles.rb
+++ scrambles.rb
@@ -1,3 +1,18 @@
+=begin
+Complete the function scramble(str1, str2) that returns true if a portion of str1 characters can be rearranged to match str2, otherwise returns false.
+
+Notes:
+
+Only lower case letters will be used (a-z). No punctuation or digits will be included.
+Performance needs to be considered
+Input strings s1 and s2 are null terminated.
+Examples
+scramble('rkqodlw', 'world') ==> True
+scramble('cedewaraaossoqqyt', 'codewars') ==> True
+scramble('katas', 'steak') ==> False
+=end
+
+
 def scramble(s1,s2)
     first_word = s1.split('').select { |item| s2.include?   item }
     first_word = first_word.sort
@@ -11,3 +26,11 @@ def scramble(s1,s2)
     end
   
   end
+
+
+# Test cases
+#puts scramble('rkqodlw','world')
+puts scramble('cedewaraaossoqqyt','codewars')
+#puts scramble('katas','steak')
+#puts scramble('scriptjava','javascript')
+#puts scramble('scriptjava','javascript')
