ae78ad2fb68e129c12e1ed749774c41a81021517
Alexandre Rojon <alexandre.rojon@gmail.com>
2022-01-31 17:04:14 +0100

    Test cases added for Applying Discounts

--- ApplyingDiscounts.rb
+++ ApplyingDiscounts.rb
@@ -11,3 +11,12 @@ Notes
 The discount is the percentage of the original price (i.e the discount of "75%" to 12 would be 9 as opposed to taking off 75% (making 3)).
 There won't be any awkward decimal numbers, only 0.5 to deal with.
 =end
+
+def get_discounts(nums, d)
+
+end
+
+
+get_discounts([2, 4, 6, 11], "50%")
+get_discounts([10, 20, 40, 80], "75%")
+get_discounts([100], "45%")
