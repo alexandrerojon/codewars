e028d9029e55487e1b4fb1581ef9193392f8c3d2
Alexandre Rojon <alexandre.rojon@gmail.com>
2022-02-07 13:40:14 +0100

    Instructions for camel case kata

--- camelcase.rb
+++ camelcase.rb
@@ -1,7 +1,17 @@
+=begin
+Write simple .camelCase method (camel_case function in PHP, CamelCase in C# or camelCase in Java) for strings. All words must have their first letter capitalized without spaces.
+
+For instance:
+
+'hello case'.camelcase => HelloCase
+'camel case word'.camelcase => CamelCaseWord
+=end
+
+
+rescue => exception
+
+end
+
 class String
     #your cool code here...
 end
-
-
-
-
