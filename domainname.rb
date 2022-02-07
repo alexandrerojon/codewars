54666972f0f0f3de1d1e516ba70684073ae0b0d9
Alexandre Rojon <alexandre.rojon@gmail.com>
2022-02-07 13:57:42 +0100

    Instructions and test cases for domain name

--- domainname.rb
+++ domainname.rb
@@ -1,11 +1,21 @@
+=begin
+Write a function that when given a URL as a string, parses out just the domain name and returns it as a string. For example:
+
+domain_name("http://github.com/carbonfive/raygun") == "github"
+domain_name("http://www.zombie-bites.com") == "zombie-bites"
+domain_name("https://www.cnet.com") == "cnet"
+=end
+
+
+
 def domain_name(url)
     # return just the domain name
 end
-  
+
 
 
 # Below test cases provided
 domain_name("http://google.com")
-domain_name("http://google.co.jp") 
+domain_name("http://google.co.jp")
 domain_name("www.xakep.ru")
 domain_name("https://youtube.com")
