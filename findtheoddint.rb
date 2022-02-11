d3f362bd6fe58885a22247be0d26b4fba18a83d8
Alexandre Rojon <alexandre.rojon@gmail.com>
2022-02-11 14:23:49 +0100

    Find odd int kata completE

--- findtheoddint.rb
+++ findtheoddint.rb
@@ -13,5 +13,14 @@ Examples
 =end
 
 def find_it(seq)
-  #your code here
+  for number in seq do
+    return number if seq.count(number).odd?
+  end
 end
+
+
+puts find_it([20,1,-1,2,-2,3,3,5,5,1,2,4,20,4,-1,-2,5])
+puts find_it([1,1,2,-2,5,2,4,4,-1,-2,5])
+puts find_it([20,1,1,2,2,3,3,5,5,4,20,4,5])
+puts find_it([10])
+puts find_it([1,1,1,1,1,1,10,1,1,1,1])
