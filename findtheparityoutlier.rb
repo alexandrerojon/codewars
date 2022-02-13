0a191fb2199e48a25c936919e1b125844ad03766
Alexandre Rojon <alexandre.rojon@gmail.com>
2022-02-13 17:29:36 +0100

    Find outlier parity kata complete with test caseS

--- findtheparityoutlier.rb
+++ findtheparityoutlier.rb
@@ -1,6 +1,8 @@
 =begin
 
-You are given an array (which will have a length of at least 3, but could be very large) containing integers. The array is either entirely comprised of odd integers or entirely comprised of even integers except for a single integer N. Write a method that takes the array as an argument and returns this "outlier" N.
+You are given an array (which will have a length of at least 3, but could be very large) containing integers.
+The array is either entirely comprised of odd integers or entirely comprised of even integers except for a
+single integer N. Write a method that takes the array as an argument and returns this "outlier" N.
 
 Examples
 [2, 4, 0, 100, 4, 11, 2602, 36]
@@ -11,8 +13,10 @@ Should return: 160 (the only even number)
 =end
 
 def find_outlier(integers)
-  puts integers
+  evens = integers.select {|item| item.even? }
+  odds = integers.select {|item| item.odd?}
+  evens.length > odds.length ? odds.first : evens.first
 end
 
-find_outlier([0, 1, 2])
-find_outlier([1, 2, 3])
+puts find_outlier([0, 1, 2])
+puts find_outlier([1, 2, 3])
