29a4475daf2582172d1561a6b91cb4de1ceec0fb
Alexandre Rojon <alexandre.rojon@gmail.com>
2022-02-19 16:47:02 +0100

    Breakcamelcase additional test cases added

--- breakcamelcase.rb
+++ breakcamelcase.rb
@@ -1,6 +1,7 @@
 =begin
 
-Complete the solution so that the function will break up camel casing, using a space between words.
+Complete the solution so that the function will break up camel casing,
+using a space between words.
 
 Example
 "camelCasing"  =>  "camel Casing"
@@ -10,8 +11,18 @@ Example
 =end
 
 def solution(string)
-  # complete the function
+  entire_word = string.split("")
+  entire_word.each_with_index do |item, index|
+    # puts item
+    # puts index
+    if item.match?(/[A-Z]/)
+      item = " " + item
+      puts item
+    end
+  end
+  puts entire_word.join()
+
 end
 
 solution('camelCasing') #'camel Casing
-solution('camelCasingTest') #'camel Casing Test'
+# solution('camelCasingTest') #'camel Casing Test'
