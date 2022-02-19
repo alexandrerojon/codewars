4d16d2eac9de04b2686797f2b44ca1aebfbbdfce
Alexandre Rojon <alexandre.rojon@gmail.com>
2022-02-19 16:35:42 +0100

    Breakcamecase instructions and test cases added

---
+++ breakcamelcase.rb
@@ -0,0 +1,17 @@
+=begin
+
+Complete the solution so that the function will break up camel casing, using a space between words.
+
+Example
+"camelCasing"  =>  "camel Casing"
+"identifier"   =>  "identifier"
+""             =>  ""
+
+=end
+
+def solution(string)
+  # complete the function
+end
+
+solution('camelCasing') #'camel Casing
+solution('camelCasingTest') #'camel Casing Test'
