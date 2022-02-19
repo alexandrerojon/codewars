4a0bb4e9359c63cdc3eafd8245782029c9f530fd
Alexandre Rojon <alexandre.rojon@gmail.com>
2022-02-19 12:43:09 +0100

    Incomplete

--- splitstrings.rb
+++ splitstrings.rb
@@ -10,10 +10,18 @@ solution('abcdef') # should return ['ab', 'cd', 'ef']
 =end
 
 def solution(str)
-
+  given_string = str.split("")
+  if given_string.length.even?
+    even_array = given_string.each_slice(2).to_a
+    return even_array
+  else
+    given_string << "_"
+    odd_array = given_string.each_slice(2).to_a
+    return odd_array
+  end
 end
 
-solution("abcdef") #["ab", "cd", "ef"]
-solution("abcdefg") #["ab", "cd", "ef", "g_"]
-solution("") #[]
-solution("abracadabra")
+puts solution("abcdef") #["ab", "cd", "ef"]
+#solution("abcdefg") #["ab", "cd", "ef", "g_"]
+#solution("") #[]
+#solution("abracadabra")
