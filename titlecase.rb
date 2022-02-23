5c1231593f8af587f0c6d7f3f28397714ae92b95
Alexandre Rojon <alexandre.rojon@gmail.com>
2022-02-23 15:48:06 +0100

    Titlecase test cases & outputs

--- titlecase.rb
+++ titlecase.rb
@@ -15,3 +15,9 @@ title_case('the quick brown fox') # should return: 'The Quick Bro
 def title_case(title, minor_words = '')
 
 end
+
+
+title_case('') #''
+title_case('a clash of KINGS', 'a an the of') #'A Clash of Kings'
+title_case('THE WIND IN THE WILLOWS', 'The In') #'The Wind in the Willows'
+title_case('the quick brown fox') #'The Quick Brown Fox'
