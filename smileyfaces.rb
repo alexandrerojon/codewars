e6abb90a104c7c5d5da2be81f707a207a70bb80b
Alexandre Rojon <alexandre.rojon@gmail.com>
2022-03-25 12:37:14 +0100

    smiley faces challenge

--- smileyfaces.rb
+++ smileyfaces.rb
@@ -1,6 +1,5 @@
 def count_smileys(arr)
-    count = 0 
-
+    count = 0
     for s in arr do
         # variables that will change throughout tests
         is_valid_emoji = false
@@ -14,30 +13,29 @@ def count_smileys(arr)
         # check the length, and if greater than two than ensure correct nose
         if s.length == 3
             nosecondition = true if s[1] == "-" || s[1] == "~"
-        elsif s.length == 2 
+        elsif s.length == 2
             nosecondition = true
-        else 
+        else
             nosecondition = false
         end
-            
         # ensure valid smiley at end
-        smilecondition = true if s[-1] == ")" || s[-1] == "D" 
+        smilecondition = true if s[-1] == ")" || s[-1] == "D"
 
         # increase the count if all conditions are met
-        count += 1 if eyescondition == true && nosecondition == true && smilecondition == true 
+        count += 1 if eyescondition == true && nosecondition == true && smilecondition == true
 
     end
     puts count
     return count
-end 
+end
 
 
 count_smileys([]) # 0
 count_smileys([":D",":~)",";~D",":)"]) # 4
 count_smileys([":)",":(",":D",":O",":;"]) # 2
-count_smileys([";]", ":[", ";*", ":$", ";-D"]) # 1 
+count_smileys([";]", ":[", ";*", ":$", ";-D"]) # 1
 count_smileys([";", ")", ";*", ":$", "8-D"]) # 0
 
-# Each smiley face must contain a valid pair of eyes. Eyes can be marked as : or ; 
+# Each smiley face must contain a valid pair of eyes. Eyes can be marked as : or ;
 # A smiley face can have a nose but it does not have to. Valid characters for a nose are - or ~
 # Every smiling face must have a smiling mouth that should be marked with either ) or D
