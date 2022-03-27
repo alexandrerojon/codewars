12925fe967822a530e9caebeaa57e7d489a7b809
Alexandre Rojon <alexandre.rojon@gmail.com>
2022-03-27 22:17:46 +0200

    PeteTheBaker refactor

--- PeteTheBaker.rb
+++ PeteTheBaker.rb
@@ -5,31 +5,26 @@
 #Examples:
 
 # must return 2
-# cakes({flour: 500, sugar: 200, eggs: 1}, {flour: 1200, sugar: 1200, eggs: 5, milk: 200}); 
+# cakes({flour: 500, sugar: 200, eggs: 1}, {flour: 1200, sugar: 1200, eggs: 5, milk: 200});
 # must return 0
-# cakes({apples: 3, flour: 300, sugar: 150, milk: 100, oil: 100}, {sugar: 500, flour: 2000, milk: 2000}); 
-
+# cakes({apples: 3, flour: 300, sugar: 150, milk: 100, oil: 100}, {sugar: 500, flour: 2000, milk: 2000});
 
 def cakes(recipe, available)
     #My code is better than Allison's
-
     have_all_ingredients = true
     n_of_cakes_possible = 0
     current_cake = Hash.new
 
     #Iteration through the ingredients needed per recipe
     recipe.each do |key, value|
-
         #statement returning false if we cannot find an ingredient required in the recipe in the available ingredients
         if !available[key]
             have_all_ingredients = false
         end
-        
-        #Push the amount of times possible we can create the current cake into local hash        
+        #Push the amount of times possible we can create the current cake into local hash
         times_possible = available[key].to_i / value.to_i
         current_cake.store(key, times_possible)
     end
-
     #Find the lowest value current cakes
     n_of_cakes_possible = current_cake.sort_by { |key, value| value }.first[1]
 
@@ -44,5 +39,3 @@ puts cakes({"apples"=>3, "flour"=>300, "sugar"=>150, "milk"=>100, "oil"=>100},{"
 puts cakes({"apples"=>3, "flour"=>300, "sugar"=>150, "milk"=>100, "oil"=>100},{"sugar"=>500, "flour"=>2000, "milk"=>2000, "apples"=>15, "oil"=>20})
 puts cakes({"eggs"=>4, "flour"=>400},{})
 puts cakes({"cream"=>1, "flour"=>3, "sugar"=>1, "milk"=>1, "oil"=>1, "eggs"=>1},{"sugar"=>1, "eggs"=>1, "flour"=>3, "cream"=>1, "oil"=>1, "milk"=>1})
-
-
