68a0fb300aea41201966c4ab898dfe2ae34b8a1a
Alexandre Rojon <alexandre.rojon@gmail.com>
2022-03-29 18:13:11 +0200

    Stringendwith challenge complete

--- stringendswith.rb
+++ stringendswith.rb
@@ -1,4 +1,5 @@
-# Complete the solution so that it returns true if the first argument(string) passed in ends with the 2nd argument (also a string).
+# Complete the solution so that it returns true if the first argument(string)
+# passed in ends with the 2nd argument (also a string).
 
 # solution('abc', 'bc') // returns true
 # solution('abc', 'd') // returns false
@@ -6,4 +7,9 @@
 
 def solution(str, ending)
   # TODO: complete
+  return str.end_with?(ending)
 end
+
+
+solution('abc', 'bc')
+solution('abc', 'd')
