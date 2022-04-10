aa7ef01a2048c2d3bdd952d506ab2ce384ce0abc
Alexandre Rojon <alexandre.rojon@gmail.com>
2022-04-10 09:46:26 +0200

    List of presents complete &* r
    refacted

--- listofpresents.rb
+++ listofpresents.rb
@@ -15,6 +15,9 @@ def how_many_gifts(max_budget, gifts)
   purse += max_budget
   amount_of_gifts = 0
 
+  # Sort the array of gifts to ensure max number of gifts can be bought
+  gifts.sort!
+
   # Iteration to go through each present, and deduct until can no longer purchase gifts
   for gift in gifts do
     if purse >= gift
@@ -30,4 +33,4 @@ end
 
 puts how_many_gifts(20, [13, 2, 4, 6, 1]) # 4
 puts how_many_gifts(0, [1]) # 0
-puts how_many_gifts(90, [87, 3, 5, 25, 1, 3, 4, 6, 20])
+puts how_many_gifts(90, [87, 3, 5, 25, 1, 3, 4, 6, 20]) # 8
