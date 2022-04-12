217bad034247fb188f38580746bc6d10d51278d0
Alexandre Rojon <alexandre.rojon@gmail.com>
2022-04-12 23:30:56 +0200

    Manhatten distance

--- manhattendistance.rb
+++ manhattendistance.rb
@@ -1,11 +1,17 @@
-# The distance formula can be used to find the distance between two points. What if we were trying to walk from point A to point B, but there were buildings in the way? We would need some other formula..but which?
+# The distance formula can be used to find the distance between two points.
+#What if we were trying to walk from point A to point B, but there were buildings in the way?
+# We would need some other formula..but which?
 
 # Manhattan Distance
-# Manhattan distance is the distance between two points in a grid (like the grid-like street geography of the New York borough of Manhattan) calculated by only taking a vertical and/or horizontal path.
+# Manhattan distance is the distance between two points in a grid (like the
+# grid-like street geography of the New York borough of Manhattan) calculated by only
+# taking a vertical and/or horizontal path.
 
-# Complete the function that accepts two points and returns the Manhattan Distance between the two points.
+# Complete the function that accepts two points and returns the Manhattan Distance
+# between the two points.
 
-# The points are arrays or tuples containing the x and y coordinate in the grid. You can think of x as the row in the grid, and y as the column.
+# The points are arrays or tuples containing the x and y coordinate in the grid.
+# You can think of x as the row in the grid, and y as the column.
 
 # Examples
 # * Input [1, 1], [1, 1] => Output 0
@@ -14,6 +20,8 @@
 
 def manhattan_distance(point_a, point_b)
   # your code here
+
+
 end
 
 
