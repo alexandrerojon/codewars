cb6058f22786956c5ee58c0f504b92b5f6d4feb8
Alexandre Rojon <alexandre.rojon@gmail.com>
2022-05-18 12:00:32 -0700

    Incrementer not finished

--- incrementer.rb
+++ incrementer.rb
@@ -1,5 +1,4 @@
 # Given an input of an array of digits, return the array with each digit incremented by its position in the array: the first digit will be incremented by 1, the second digit by 2, etc. Make sure to start counting your positions from 1 ( and not 0 ).
-
 # Your result can only contain single digit numbers, so if adding a digit with its position gives you a multiple-digit number, only the last digit of the number should be returned.
 # Notes:
 # return an empty array if your array is empty
@@ -11,11 +10,19 @@
 #                                        #  9+3 = 12  -->  2
 
 def incrementer(nums)
-  # code goes here
+  final_array = []
+  nums.each_with_index do |number, index|
+    number += (index + 1)
+    if number > 9
+      number = (number - 10)
+    end
+    final_array << number
+  end
+  return final_array
 end
 
 
-incrementer([1,2,3])
-incrementer([4,6,7,1,3])
-incrementer([3,6,9,8,9])
-incrementer([1,2,3,4,5,6,7,8,9,9,9,9,9,8])
+#incrementer([1,2,3])
+puts incrementer([4,6,7,1,3])
+# incrementer([3,6,9,8,9])
+# incrementer([1,2,3,4,5,6,7,8,9,9,9,9,9,8])
