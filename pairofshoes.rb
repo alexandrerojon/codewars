dcfea5279a06605351fbf4749896b23c43937c3d
Alexandre Rojon <alexandre.rojon@gmail.com>
2022-08-28 23:35:33 -0700

    saving work

--- pairofshoes.rb
+++ pairofshoes.rb
@@ -18,45 +18,46 @@
 # the output should be false.
 
 def pair_of_shoes(shoes)
-  return false if shoes.length.odd? || shoes.length < 4
+  return false if shoes.length.odd?
 
   shoes.each do |shoe|
-
     # Switch statement
-    which_foot = 0
-    which_foot = 1 if shoe[0] == 0
+    opposite_foot = 0
+    opposite_foot = 1 if shoe[0] == 0
 
     # Current comparison array
     current_foot = []
-    current_foot << which_foot
+    current_foot << shoe[0]
     current_foot << shoe[1]
 
-    return false if shoes.count(current_foot) < 1
-    # if shoes.count(current_foot) >= 1
-    #   puts "found it!"
+    # Desired foot
+    desired_foot = []
+    desired_foot = opposite_foot
+    desired_foot << shoe[1]
 
-    # else
-    #   puts " did not find, error"
-    # end
-    # puts "---"
-
+    if shoes.count(desired_foot) < 1
+      return false
+    else
+      shoes.delete(current_foot)
+      shoes.delete(desired_foot)
+    end
 
   end
   return true
 end
 
 # Test cases below
-puts pair_of_shoes([[0,21],[1,23],[1,21],[0,23]]) #true
-puts pair_of_shoes([[0,21],[1,23],[1,21],[1,23]]) #false
-puts pair_of_shoes([[0,23],[1,21],[1,23],[0,21],[1,22],[0,22]]) #true
-puts pair_of_shoes([[0,23],[1,21],[1,23],[0,21]]) #true
-puts pair_of_shoes([[0,23],[1,21],[1,22],[0,21]]) #false
-puts pair_of_shoes([[0,23]]) #false
+# puts pair_of_shoes([[0,21],[1,23],[1,21],[0,23]]) #true
+# puts pair_of_shoes([[0,21],[1,23],[1,21],[1,23]]) #false
+# puts pair_of_shoes([[0,23],[1,21],[1,23],[0,21],[1,22],[0,22]]) #true
+# puts pair_of_shoes([[0,23],[1,21],[1,23],[0,21]]) #true
+# puts pair_of_shoes([[0,23],[1,21],[1,22],[0,21]]) #false
+# puts pair_of_shoes([[0,23]]) #false
 
 # Additional more advanced test cases
-pair_of_shoes([[0,23],[1,23]]) #true
-pair_of_shoes([[0,23],[1,23],[1,23],[0,23]]) #true
-pair_of_shoes([[0,23],[1,22]]) #false
-pair_of_shoes([[0,23],[1,23],[1,23],[0,23],[0,23],[0,23]]) #false
-pair_of_shoes([[0,21],[1,23],[0,21],[1,23]]) #false
-pair_of_shoes([[1, 42], [0, 42], [1, 33], [0, 33]]) #true
+puts pair_of_shoes([[0,23],[1,23]]) #true
+puts pair_of_shoes([[0,23],[1,23],[1,23],[0,23]]) #true
+puts pair_of_shoes([[0,23],[1,22]]) #false
+puts pair_of_shoes([[0,23],[1,23],[1,23],[0,23],[0,23],[0,23]]) #false
+puts pair_of_shoes([[0,21],[1,23],[0,21],[1,23]]) #false
+puts pair_of_shoes([[1, 42], [0, 42], [1, 33], [0, 33]]) #true
