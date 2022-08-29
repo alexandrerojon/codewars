7b68a36017a289fdae00b8218b9fe7824abf9589
Alexandre Rojon <alexandre.rojon@gmail.com>
2022-08-28 21:19:32 -0700

    pair of shoes

--- pairofshoes.rb
+++ pairofshoes.rb
@@ -1,7 +1,8 @@
 # Yesterday you found some shoes in your room. Each shoe is described by two values:
 # type indicates if it's a left or a right shoe;
 # size is the size of the shoe.
-# Your task is to check whether it is possible to pair the shoes you found in such a way that each pair consists of a right and a left shoe of an equal size.
+# Your task is to check whether it is possible to pair the shoes you found in such a
+# way that each pair consists of a right and a left shoe of an equal size.
 # For:
 
 # shoes = [[0, 21],
@@ -18,10 +19,11 @@
 
 def pair_of_shoes(shoes)
   # Combine all the arrays into a single larger one
-  combined_shoes = shoes.concat()
+  combined_shoes = shoes.join().split('')
 
   # Iterate through each item in the combined darray
   combined_shoes.each { |shoe|
+
     return false if combined_shoes.count(shoe).odd?
   }
 
@@ -35,8 +37,8 @@ end
 
 # Test cases below
 puts pair_of_shoes([[0,21],[1,23],[1,21],[0,23]]) #true
-# pair_of_shoes([[0,21],[1,23],[1,21],[1,23]]) #false
-# pair_of_shoes([[0,23],[1,21],[1,23],[0,21],[1,22],[0,22]]) #true
+puts pair_of_shoes([[0,21],[1,23],[1,21],[1,23]]) #false
+puts pair_of_shoes([[0,23],[1,21],[1,23],[0,21],[1,22],[0,22]]) #true
 # pair_of_shoes([[0,23],[1,21],[1,23],[0,21]]) #true
 # pair_of_shoes([[0,23],[1,21],[1,22],[0,21]]) #false
 # pair_of_shoes([[0,23]]) #false
