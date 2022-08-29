b3e03a07addf719a2aa2a07c7b76e684588e80e6
Alexandre Rojon <alexandre.rojon@gmail.com>
2022-08-28 23:07:54 -0700

    Pair of shoes

--- pairofshoes.rb
+++ pairofshoes.rb
@@ -18,27 +18,23 @@
 # the output should be false.
 
 def pair_of_shoes(shoes)
-  # Combine all the arrays into a single larger one
-  combined_shoes = shoes.join().split('')
+  return false if shoes.length.odd? || shoes.length < 4
+  shoes.each do |shoe|
+    # Switch statement
+    which_foot = 0
+    which_foot == 1 if shoe.zero?
 
-  # Iterate through each item in the combined darray
-  combined_shoes.each { |shoe|
-
-    return false if combined_shoes.count(shoe).odd?
-  }
-
+    puts shoe
+    puts "----"
+    return false if shoes.find
+  end
   return true
-
-
-
-
-
 end
 
 # Test cases below
 puts pair_of_shoes([[0,21],[1,23],[1,21],[0,23]]) #true
-puts pair_of_shoes([[0,21],[1,23],[1,21],[1,23]]) #false
-puts pair_of_shoes([[0,23],[1,21],[1,23],[0,21],[1,22],[0,22]]) #true
-# pair_of_shoes([[0,23],[1,21],[1,23],[0,21]]) #true
-# pair_of_shoes([[0,23],[1,21],[1,22],[0,21]]) #false
+# puts pair_of_shoes([[0,21],[1,23],[1,21],[1,23]]) #false
+# puts pair_of_shoes([[0,23],[1,21],[1,23],[0,21],[1,22],[0,22]]) #true
+# puts pair_of_shoes([[0,23],[1,21],[1,23],[0,21]]) #true
+# puts pair_of_shoes([[0,23],[1,21],[1,22],[0,21]]) #false
 # pair_of_shoes([[0,23]]) #false
