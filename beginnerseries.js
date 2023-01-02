7dafcd75f3d0255ec427aea0e7745e27f6ceeb5e
Alexandre Rojon <alexandre.rojon@gmail.com>
2023-01-02 21:29:38 +0100

    JS challenge

--- beginnerseries.js
+++ beginnerseries.js
@@ -1,4 +1,6 @@
-/*Given two integers a and b, which can be positive or negative, find the sum of all the integers between and including them and return it. If the two numbers are equal return a or b.
+/*Given two integers a and b, which can be positive or negative,
+find the sum of all the integers between and including them and return it.
+If the two numbers are equal return a or b.
 
 Note: a and b are not ordered!
 
@@ -13,10 +15,10 @@ Examples (a, b) --> output (explanation)
 
 function getSum( a,b )
 {
-   //Good luck!
-
+  return (a == b ? a : a + b);
 }
 
 
-getSum(0,-1) //-1
+console.log(getSum(0,-1)); //-1
 getSum(0,1) //1
+console.log(getSum(-406,-525)); //-931
